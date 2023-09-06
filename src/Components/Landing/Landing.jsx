d4c653f498723645d47070e50849779b382f59da
Elkema89 <elkema89gmail.com>
2023-09-06 18:30:44 -0300

    landing terminado

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 const Landing = ()=>{
 
+    const img = "https://res.cloudinary.com/dt0bfmvvy/image/upload/v1694005326/Matsu/vec5yi4bwvqr3it1nwft.jpg"
     const navigate = useNavigate()
     const handlerClick =()=>{
         navigate("/home")
@@ -12,8 +13,11 @@ const Landing = ()=>{
 
     return (
         <div className={style.container}>
+            <div className={style.opacity}>
+            <img src={img} alt="logo" className={style.img} />
             <h1 className={style.h1}>BIENVENIDO</h1>
             <Button name="INGRESAR" action={handlerClick} />
+            </div>
         </div>
     )
 }
