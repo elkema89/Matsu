120e3976b2e2070226dc20b6e19bb796443e807f
Elkema89 <elkema89gmail.com>
2023-09-12 19:08:37 -0300

    SideBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import style from "./NavBar.module.css"
 import { NavLink } from "react-router-dom";
 
+
 const NavBar = () => {
 
     return (
         <div className={style.container}>
-            <h1 className={style.h1}>MATSU</h1>
+            <h1>MATSU</h1>
             <ul className={style.opciones}>
-                <NavLink className={style.opcion}>Productos</NavLink>
+                <NavLink className={style.opcion}>Home</NavLink>
                 <NavLink className={style.opcion}>Sobre Nosotros</NavLink>
                 <NavLink className={style.opcion}>Registrarse</NavLink>
                 <NavLink className={style.opcion}>Iniciar Sesion</NavLink>
--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,8 +1,15 @@
 import NavBar from "../../Components/NavBar/NavBar"
+import Loading from "../../Components/Loading/Loading"
+import SideBar from "../../Components/SideBar/SideBar"
+import style from "./Home.module.css"
 const Home =()=>{
     return (
-        <div>
+        <div className={style.container}>
             <NavBar />
+            <div className={style.home}>
+            <SideBar />
+            <Loading />
+            </div>
         </div>
     )
 }
