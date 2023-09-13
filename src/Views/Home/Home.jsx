1170a34523d10fb4a6406964b53cf20490c75d76
Elkema89 <elkema89gmail.com>
2023-09-13 13:33:34 -0300

    home arreglado

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,16 +1,15 @@
-<<<<<<<<< Temporary merge branch 1
 import NavBar from "../../Components/NavBar/NavBar"
+import Loading from "../../Components/Loading/Loading"
+import SideBar from "../../Components/SideBar/SideBar"
+import style from "./Home.module.css"
 const Home =()=>{
     return (
         <div className={style.container}>
             <NavBar />
-=========
-
-const Home =()=>{
-    return (
-        <div>
-            
->>>>>>>>> Temporary merge branch 2
+            <div className={style.home}> 
+                <SideBar />
+                <Loading />
+            </div>
         </div>
     )
 }
