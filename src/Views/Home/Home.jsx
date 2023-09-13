6683dc7183cc1b33f7646fc2cdae0263cc2bc663
Elkema89 <elkema89gmail.com>
2023-09-13 13:27:43 -0300

    git lcdtm

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,16 +1,9 @@
-<<<<<<<<< Temporary merge branch 1
 import NavBar from "../../Components/NavBar/NavBar"
-const Home =()=>{
-    return (
-        <div>
-            <NavBar />
-=========
 
 const Home =()=>{
     return (
         <div>
-            
->>>>>>>>> Temporary merge branch 2
+            <NavBar />
         </div>
     )
 }
